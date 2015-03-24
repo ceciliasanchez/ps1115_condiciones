6780ec99a75d80b5180f31eab9cf35cf0c10adad
ceciliasanchez <cecielimacas2910@gmail.com>
2015-03-23 20:54:31 -0500

    cambios test

--- src/test/TestMain.java
+++ src/test/TestMain.java
@@ -3,8 +3,6 @@ package test;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
